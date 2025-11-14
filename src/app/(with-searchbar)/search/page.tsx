6e6d0f1c532ded1c57da83c0638c1691b319d146
Dang <wheodnjs2@naver.com>
2025-11-15 01:05:48 +0900

    스테틱 페이지 2

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -12,7 +12,10 @@ export default async function Page({
     `${
       process.env.NEXT_PUBLIC_API_SERVER_URL
     }/book/search?q=${encodeURIComponent(q)}`,
-    { cache: 'no-store' }
+
+    // searchParams 를 사용하기 때문에 스테틱 페이지로 만들 수 없지만
+    // 캐시를 저장시킴으로서 페칭을 조금 더 빠르게 로드 가능
+    { cache: 'force-cache' }
   );
 
   if (!response.ok) {
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,5 +1,19 @@
 import { BookData } from '@/types';
 import style from './page.module.css';
+import { notFound } from 'next/navigation';
+
+// 아래 만들어진 1,2,3 이외에는 모두
+// 404 페이지로 보낸다.
+// export const dynamicParams = false;
+
+// id 1,2,3 만 정적페이지로 생성
+// 1,2,3 이외의 페이지를 통해서 들어와도
+// 처음엔 로드하는 시간이 걸리지만
+// 정적인 페이지(html)가 만들어져서
+// 추후에 들어올때는 페이지가 빠르게 로드 된다.
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }];
+}
 
 export default async function Page({
   params,
@@ -9,11 +23,14 @@ export default async function Page({
   const { id } = await params;
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`,
-    { cache: 'no-store' }
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
   );
 
   if (!response.ok) {
+    // 404 에러페이지
+    if (response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
 
